e064de94847e9c46ac598c5600c1e96fde2dbac3
ASO <Alvarito1624@gmail.com>
2025-06-27 00:32:44 -0400

    calndarer

--- src/components/card-details.tsx
+++ src/components/card-details.tsx
@@ -1,4 +1,4 @@
-import { faCalendar, type IconDefinition } from "@fortawesome/free-solid-svg-icons"
+import { type IconDefinition } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import type { FC } from "react"
 
